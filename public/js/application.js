7ff591ed4009df08965d342f2af7d7a933189fa0
charly palencia <ingcharlypalencia@gmail.com>
2012-08-23 23:19:39 -0500

    bugfix- datepicker formatdate

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,7 @@
 $(function(){
 
-	$(".date").datepicker({ dateFormat: "yy-dd-mm" });
+	$(".date").datepicker({ dateFormat: "yy-mm-dd" });
 	$(".time").timepicker({});
-	$(".datetime").datetimepicker({ dateFormat: "yy-dd-mm" });
+	$(".datetime").datetimepicker({ dateFormat: "yy-mm-dd" });
 
 })
