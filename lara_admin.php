9526e214640f74073cb209afb72b0da0e3f354fd
charly palencia <ingcharlypalencia@gmail.com>
2012-08-23 21:50:21 -0500

    delete test values

--- lara_admin.php
+++ lara_admin.php
@@ -6,15 +6,6 @@ class LaraAdmin{
 		// Config::set('laraAdmin.models', array() );
 		Config::set('laraAdmin.models', array(
 				//example "Test", "User"
-				"usuario",
-				"OpcionUsuario",
-				"establecimiento",
-				"tipoEstablecimiento",
-				"estadoPedido",
-				"prioridadPedido",
-				"Pedido",
-				"tipoPedido",
-				"Promocion"
 			));
 		Config::set('laraAdmin.title', "Framework Drakar");
 		
