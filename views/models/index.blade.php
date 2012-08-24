6b0beb1416d81f81989ea4ff2f4cbc06c3ea0f71
charly palencia <ingcharlypalencia@gmail.com>
2012-08-24 07:57:47 -0500

    [bugfix] fixed problem with calendar datepicker and error validation to seach created_at in filters

--- views/models/index.blade.php
+++ views/models/index.blade.php
@@ -112,8 +112,8 @@
 						<label class=" label" for="q_created_at_gte">Created At
 						</label>
 						<input class="datetime" id="q_created_at_gte" max="10" name="created_at_gte" size="12" type="text" value="{{ Input::get("created_at_gte") }}" />
-						<span class="datetime">-</span>
-						<input class="datepicker" id="q_created_at_lte" max="10" name="created_at_lte" size="12" type="text" value="{{ Input::get("created_at_lte") }}" /></div><div class="buttons"><input id="q_submit" name="commit" type="submit" value="Filter" />
+						<span class="seperator">-</span>
+						<input class="datetime" id="q_created_at_lte" max="10" name="created_at_lte" size="12" type="text" value="{{ Input::get("created_at_lte") }}" /></div><div class="buttons"><input id="q_submit" name="commit" type="submit" value="Filter" />
 						<a href="#" class="clear_filters_btn">Clear Filters</a>
 						<input id="order" name="order" type="hidden" value="{{$sort_options["column_order"]}}_{{$sort_options["sort_direction"]}}" />
 						<input id="scope" name="scope" type="hidden" /></div>
