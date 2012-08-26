44ea1b73491889c3f7751d7c7d1572820c63251e
charly palencia <ingcharlypalencia@gmail.com>
2012-08-25 19:19:06 -0500

    [bugfix] change link clear filter for a butotn reset type

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,13 @@ $(function(){
 	$(".time").timepicker({});
 	$(".datetime").datetimepicker({ dateFormat: "yy-mm-dd" });
 
+	window.resetForm= function($form) {
+    $form.find('input:text, input:password, input:file, select, textarea').val('').attr("value","");
+    $form.find('input:radio, input:checkbox')
+         .removeAttr('checked').removeAttr('selected');
+	}
+
+	$("input[type='reset']").click(function(){
+		resetForm( $(this).closest("form") );
+	});
 })
